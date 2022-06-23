210a568d2c0c69f28af4deeaac1ed36ec6067d31
NerdFromHell <izilovy@gmail.com>
2022-06-23 10:47:34 +0300

    email validators and button design

--- src/app/components/email-form/email-form.component.ts
+++ src/app/components/email-form/email-form.component.ts
@@ -5,11 +5,24 @@ import { Component, OnInit } from '@angular/core';
   templateUrl: './email-form.component.html',
   styleUrls: ['./email-form.component.scss']
 })
-export class EmailFormComponent implements OnInit {
+export class EmailFormComponent {
 
   constructor() { }
 
-  ngOnInit(): void {
-  }
+  validate(evt: Event){
+    evt.preventDefault();
+    var email: string = (<HTMLInputElement>document.getElementById('email')).value;
+    var message: string = (<HTMLInputElement>document.getElementById('message')).value;
+    var regx:RegExp = /^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/;
+    if(!regx.test(email)){
+      alert("invalid email adress");
+      return;
+    }
+    if(message.length === 0){
+      alert("empty message :<");
+      return;
+    }
+    alert('nice')
 
+  }
 }
